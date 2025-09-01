eb9ffb009bbd32a194cb0c177846fd6e48ee4973
Jan Kowalski <neytro@o2.pl>
2025-09-01 22:10:56 +0200

    add second radio button

--- Interactive PDF/Interactive PDF/Program.cs
+++ Interactive PDF/Interactive PDF/Program.cs
@@ -39,22 +39,22 @@ class Example
             Paragraph mainName = new Paragraph("Vor- und Nachname:");
             mainName.SetFontSize(10);
             PdfTextFormField mainNameField = new TextFormFieldBuilder(pdf, "mainNameField")
-               .SetWidgetRectangle(new Rectangle(180, 670, 200, 18)).CreateText();
+               .SetWidgetRectangle(new Rectangle(180, 670, 370, 18)).CreateText();
 
             Paragraph street = new Paragraph("Straße und Hausnummer:");
             street.SetFontSize(10);
             PdfTextFormField streetField = new TextFormFieldBuilder(pdf, "streetField")
-               .SetWidgetRectangle(new Rectangle(180, 647, 200, 18)).CreateText();
+               .SetWidgetRectangle(new Rectangle(180, 647, 370, 18)).CreateText();
 
             Paragraph home = new Paragraph("PLZ und Wohnort:");
             home.SetFontSize(10);
             PdfTextFormField homeField = new TextFormFieldBuilder(pdf, "homeField")
-               .SetWidgetRectangle(new Rectangle(180, 624, 200, 18)).CreateText();
+               .SetWidgetRectangle(new Rectangle(180, 624, 370, 18)).CreateText();
 
             Paragraph userId = new Paragraph("Personalausweisnummer:");
             userId.SetFontSize(10);
             PdfTextFormField userIdField = new TextFormFieldBuilder(pdf, "userIdField")
-               .SetWidgetRectangle(new Rectangle(180, 601, 200, 18)).CreateText();
+               .SetWidgetRectangle(new Rectangle(180, 601, 370, 18)).CreateText();
 
             Paragraph proofData = new Paragraph("Stimmen die angegebenen Daten aus der Selbstauskunft mit der Wirklichkeit überein?");
             proofData.SetFontSize(10);
@@ -92,10 +92,57 @@ class Example
                    .SetBorderWidth(1)
                    .SetBorderColor(ColorConstants.BLACK)
                    .SetBackgroundColor(ColorConstants.WHITE);
-            
+
             proofRadioGroup.AddKid(fieldJa);
             proofRadioGroup.AddKid(fieldNein);
             proofRadioGroup.AddKid(fieldPart);
+
+            Paragraph newLine = new Paragraph("");
+
+            Paragraph explanation = new Paragraph("Erläuterung:");
+            explanation.SetFontSize(10);
+            PdfTextFormField explanationField = new TextFormFieldBuilder(pdf, "explanationField")
+               .SetWidgetRectangle(new Rectangle(180,530, 370, 18)).CreateText();
+
+            Paragraph permisson = new Paragraph("Liegt eine schriftliche Einverständniserklärung des Vermieters zur Hundehaltung vor?");
+            permisson.SetFontSize(10);
+
+            Paragraph jaTextPermisionData = new Paragraph("Ja");
+            jaTextPermisionData.SetFontSize(10);
+            jaTextPermisionData.SetFixedPosition(1, 57, 480, 20);
+
+            Paragraph neinTextPermisionData = new Paragraph("Nein, wird nachgereicht");
+            neinTextPermisionData.SetFontSize(10);
+            neinTextPermisionData.SetFixedPosition(1, 120, 480, 200);
+
+            Paragraph descriptionTextPermisionData = new Paragraph("Eigentum");
+            descriptionTextPermisionData.SetFontSize(10);
+            descriptionTextPermisionData.SetFixedPosition(1, 270, 480, 200);
+
+            RadioFormFieldBuilder permisionBuilder = new RadioFormFieldBuilder(pdf, "permisionbuilder");
+            PdfButtonFormField permisionRadioGroup = permisionBuilder.CreateRadioGroup();
+            Rectangle permisionrectJa = new Rectangle(37, 480, 15, 15);
+            Rectangle permisionrectNein = new Rectangle(100, 480, 15, 15);
+            Rectangle permisionrectPart = new Rectangle(250, 480, 15, 15);
+            PdfFormAnnotation permisionfieldJa = permisionBuilder
+                    .CreateRadioButton("permisionrectJa", permisionrectJa)
+                    .SetBorderWidth(1)
+                    .SetBorderColor(ColorConstants.BLACK)
+                    .SetBackgroundColor(ColorConstants.WHITE);
+            PdfFormAnnotation permisionfieldNein = permisionBuilder
+                    .CreateRadioButton("permisionrectNein", permisionrectNein)
+                    .SetBorderWidth(1)
+                    .SetBorderColor(ColorConstants.BLACK)
+                    .SetBackgroundColor(ColorConstants.WHITE);
+            PdfFormAnnotation permisionfieldPart = permisionBuilder
+                   .CreateRadioButton("permisionrectPart", permisionrectPart)
+                   .SetBorderWidth(1)
+                   .SetBorderColor(ColorConstants.BLACK)
+                   .SetBackgroundColor(ColorConstants.WHITE);
+
+            permisionRadioGroup.AddKid(permisionfieldJa);
+            permisionRadioGroup.AddKid(permisionfieldNein);
+            permisionRadioGroup.AddKid(permisionfieldPart);
 
             PdfAcroForm form = PdfFormCreator.GetAcroForm(pdf, true);
 
@@ -111,6 +158,14 @@ class Example
             document.Add(jaTextProofData);
             document.Add(neinTextProofData);
             document.Add(descriptionTextProofData);
+            document.Add(newLine);
+            document.Add(newLine);
+            document.Add(newLine);
+            document.Add(explanation);
+            document.Add(permisson);
+            document.Add(jaTextPermisionData);
+            document.Add(neinTextPermisionData);
+            document.Add(descriptionTextPermisionData);
 
             form.AddField(nameField);
             form.AddField(dateField);
@@ -119,6 +174,8 @@ class Example
             form.AddField(homeField);
             form.AddField(userIdField);
             form.AddField(proofRadioGroup);
+            form.AddField(explanationField);
+            form.AddField(permisionRadioGroup);
 
             document.Close();
 
